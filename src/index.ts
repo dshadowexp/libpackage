836e7ffa37b2030d4486608826f5d0697f1382bf
Annonysam <annonysam@Awuahs-MacBook-Air.local>
2024-10-26 22:00:54 -0400

    added verify jwt to the index file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { logRequests, asyncHandler, authenticate, authorize, notFound, error } from "./utils/middlewares";
 export { firstLetterUppercase, lowerCase, toUpperCase, isEmail } from "./utils/string";
 export { getGeoIndexFromLatLng, findConnectedCells } from "./services/h3";
+export { verifyJWT } from "./utils/jwt";
 export { winstonLogger } from "./utils/logger";
 export { KafkaClient, KafkaConsumer, KafkaProducer, KafkaMessageProcessor, ProcessorMessageData } from "./services/kafka";
 import RedisDB from "./services/redis";

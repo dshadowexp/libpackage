77426cfd9324504c78d976667372a443659e91ac
Annonysam <annonysam@Awuahs-MacBook-Air.local>
2024-10-26 21:40:59 -0400

    updated the user things

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -3,7 +3,7 @@ import { Request } from "express";
 declare global {
     namespace Express {
         interface Request {
-            user?: any;
+            user?: { role: string, uid: string };
         }
     }
 }
